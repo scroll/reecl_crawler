63ae60d5903d05c26dd1a3a2c94a15dd735523be
Marin Petrov <marin.petrov@hackandpaint.com>
2018-09-10 13:59:09 +0300

    Getting all info from a company

--- crawler.py
+++ crawler.py
@@ -3,12 +3,10 @@ import csv
 from datetime import datetime
 import requests
 
-
 page = 'http://reecl.org/category/%D0%B4%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D1%87%D0%B8%D1%86%D0%B8-%D0%B8-%D0%B8%D0%B7%D0%BF%D1%8A%D0%BB%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D0%B8/%D0%B8%D0%B7%D0%BF%D1%8A%D0%BB%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D0%B8-%D0%BF%D1%80%D0%BE%D0%B7%D0%BE%D1%80%D1%86%D0%B8/page/'
 
-output = '/home/marin.petrov/workspace/reecl_crawler/output.csv'
-
-output_data = {}
+output_file = '/home/marin.petrov/workspace/reecl_crawler/output.csv'
+output_data = []
 
 def extract_email(mailtos):
     for i in mailtos:
@@ -20,7 +18,8 @@ def extract_email(mailtos):
         
         return str2
 
-for i in range(1): # random large enough number of pages
+print 'Starting to crawl...'
+for i in range(200): # random large enough number of pages
     r = requests.get('{}{}/'.format(page,i))
     if r.status_code == 404:
         break
@@ -30,23 +29,23 @@ for i in range(1): # random large enough number of pages
     # These are all the list elements on the page
     li = ul.find_all('li')
 
-    for elem in li:
-        print '--------------------'
-        company_name = elem.find('a', rel="bookmark").text
-        print company_name
-        company_email = elem.select('a[href^=mailto:]')
-        print extract_email(company_email)
-        company_websites = elem.select('a[href^=http:]')
-        print company_website
+    # We iterate through each company
+    for company in li:
+        company_row = []
+        company_name = company.find('a', rel="bookmark").text
+        company_row.append(company_name)
+        company_email = company.select('a[href^=mailto:]')
+        company_row.append(extract_email(company_email))
+        company_websites = company.select('a[href^=http:]')
+        for site in company_websites:
+            company_row.append(site['href'])
 
-        # for c in company_name:
-        #     print c
-            
-            # output_data.add(all_links[-1]['href'])
+        output_data.append(company_row)
+        
+print 'Dumping to file...'
+with open(output_file, 'a') as csv_file:
+    writer = csv.writer(csv_file)
+    for company_row in output_data:
+        writer.writerow([unicode(s).encode("utf-8") for s in company_row])
+print 'Done!'
 
-
-# print output_data
-# with open(output, 'a') as csv_file:
-#     writer = csv.writer(csv_file)
-#     for row in output_data:
-#         writer.writerow([row])
